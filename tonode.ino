f13cc2ebfa457f9175386f4696daf4f98dc2e71e
eliezerpedro <36872021+eliezerpedro@users.noreply.github.com>
2019-05-02 16:53:02 -0300

    Update tonode.ino

--- tonode.ino
+++ tonode.ino
@@ -1,135 +1,108 @@
 //https://www.youtube.com/watch?v=V02StJVbslE
 
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
 #include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson/releases/tag/v5.0.7
+
 
 //-------- Customise these values -----------
 const char* ssid = "IBMHackatruckIoT";
 const char* password = "IOT2017IBM";
 
-#define ORG "m5j83l"
-#define DEVICE_TYPE "padlock"
-#define DEVICE_ID "padlock0001"
-#define TOKEN "12345678"
+#define ORG "cd7gkk"
+#define DEVICE_TYPE "padlockbeta"
+#define DEVICE_ID "padlock01"
+#define TOKEN "123456789"
 //-------- Customise the above values --------
 
 char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
 char authMethod[] = "use-token-auth";
 char token[] = TOKEN;
 char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
-
 const char eventTopic[] = "iot-2/evt/status/fmt/json";
 const char cmdTopic[] = "iot-2/cmd/led/fmt/json";
 
+int cont = 0;                         // Realiza uma contagem contínua
 WiFiClient wifiClient;
-void callback(char* topic, byte* payload, unsigned int payloadLength) {
+
+void callback(char* topic, byte* payload, unsigned int payloadLength)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < payloadLength; i++) {
-    Serial.print((char)payload[i]);
+  if ((char)payload[0] == '0')
+  { // Caso receba o caractere 0
+    digitalWrite(BUILTIN_LED, LOW);        // Desliga o LED
+    Serial.println("LOW");
+    Serial.println("pegou low");
   }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if (payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  if ((char)payload[0] == '1')
+  { // Caso receba o caractere 1
+    digitalWrite(BUILTIN_LED, HIGH);        // Liga o LED
+    Serial.println("HIGH");
+    Serial.println("pegou HIGh");
   }
-
 }
 PubSubClient client(server, 1883, callback, wifiClient);
 
-int publishInterval = 5000; // 5 seconds//Send adc every 5sc
-long lastPublishMillis;
-
-void setup() {
-  Serial.begin(9600); Serial.println();
-  pinMode(LED_BUILTIN, OUTPUT);
-  wifiConnect();
-  mqttConnect();
+void setup()
+{
+  Serial.begin(9600);                // Taxa de transmissão 9600 ou 115200 dBs
+  pinMode(13, OUTPUT);              // LED13 que será acionado pelo IBM
+  pinMode(2, OUTPUT);               // LED2 indica falha na conexão com a internet
+  client.setCallback(callback);      // Ajusta a função de retorno
+  wifiConnect();                     // Cria o void wifiConnect
+  mqttConnect(); 
+  // Cria o void mqttConnect
 }
-
-void loop() {
-  if (millis() - lastPublishMillis > publishInterval) {
-    publishData();
-    lastPublishMillis = millis();
-  }
-
-  if (!client.loop()) {
-    mqttConnect();
-  }
-}
-
-void wifiConnect() {
-  Serial.print("Connecting to "); Serial.print(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+void wifiConnect()
+{ // Função void wifiConnect
+  Serial.print("Conectando a Rede ");
+  Serial.print(ssid);                // Indica a Rede que o ESP32 irá se conectar
+  WiFi.begin(ssid, password);        // Conecta ao ssid e o password configurado
+  while (WiFi.status() != WL_CONNECTED)
+  { // Enquanto estiver desconectado
+    delay(500);                      // Aguarda 500 milissegundos
     Serial.print(".");
   }
-  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
-
+  Serial.print("Wi-Fi conectado, Endereço de IP: ");
+  Serial.println(WiFi.localIP());     // Indica o endereço de IP
 }
-
-void mqttConnect() {
-  if (!!!client.connected()) {
-    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
-    while (!!!client.connect(clientId, authMethod, token)) {
+void mqttConnect()
+{ // Função void mqttConnect
+  if (!!!client.connected())
+  {
+    Serial.print("Reconectando MQTT do cliente ");
+    Serial.println(server);                       // Indica o endereço do servidor
+    while (!!!client.connect(clientId, authMethod, token) )
+    {
       Serial.print(".");
       delay(500);
     }
-    if (client.subscribe(cmdTopic)) {
-      Serial.println("subscribe to responses OK");
-    } else {
-      Serial.println("subscribe to responses FAILED");
+    if (client.subscribe(cmdTopic))
+    {
+      Serial.println("Resposta OK");              // Se a resposta for OK
+      digitalWrite(2, LOW);                            // LED 2 OFF
     }
-    Serial.println();
+    else
+    {
+      Serial.println("Resposta FALHOU");   // Se a resposta falhar
+      digitalWrite(2, HIGH);                          // LED 2 ON
+    }
   }
 }
-
-
-void publishData() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-
-  String payload = "{\"d\":{\"adc\":";
-  payload += String(sensorValue, DEC);
-  payload += "}}";
-
-  Serial.print("Sending payload: "); Serial.println(payload);
-
-  if (client.publish(eventTopic, (char*) payload.c_str())) {
-    Serial.println("Publish OK");
-  } else {
-    Serial.println("Publish FAILED");
+void loop()
+{
+  if (!client.loop())
+  {
+    mqttConnect();
   }
+  String payload = "{\"d\":{\"adc\":";      // Inicia uma String associando ao endereço
+  payload += cont;                          // Atribui o valor de leitura de cont a String
+  payload += "}}";                          // Finaliza a String
+  Serial.print("Enviando payload: ");
+  Serial.println(payload);                  // Escreve a String no monitor Serial
+  //client.publish(eventTopic, (char*) payload.c_str() );  // Publica a String
+  cont ++;                                  // Aumenta em 1 o valor de cont
+  delay(500);                               // Aguarda 500 milissegundos
 }
-
-/*Servo servo;
-
-void setup() {
-
-    servo.attach(5); //D1
-    servo.write(0);
-
-    delay(2000);
-
-}
-
-void loop() {
-
-    servo.write(180);
-
-    delay(100);
-
-    servo.write(0);
-
-    delay(100);
-
-}*/
