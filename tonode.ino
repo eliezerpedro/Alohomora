2cf8be6b898e9f0845c01c9f98c1d5271b430a67
eliezerpedro <36872021+eliezerpedro@users.noreply.github.com>
2019-05-02 09:41:51 -0300

    Ajustes no método do protocolo MQTT

--- tonode.ino
+++ tonode.ino
@@ -20,165 +20,95 @@ char authMethod[] = "use-token-auth";
 char token[] = TOKEN;
 char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
 
-const char publishTopic[] = "iot-2/evt/status/fmt/json";
-const char responseTopic[] = "iotdm-1/response";
-const char manageTopic[] = "iotdevice-1/mgmt/manage";
-const char updateTopic[] = "iotdm-1/device/update";
-const char rebootTopic[] = "iotdm-1/mgmt/initiate/device/reboot";
-const char fauCommandTopic[] = "iot-2/cmd/faucommand/fmt/json";
+const char eventTopic[] = "iot-2/evt/status/fmt/json";
+const char cmdTopic[] = "iot-2/cmd/led/fmt/json";
 
-void callback(char*, byte*, unsigned int);
-WiFiClientSecure wifiClient;
-PubSubClient client(server, 8883, callback, wifiClient);
+WiFiClient wifiClient;
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < payloadLength; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 
-int publishInterval = 5000; // 5 seconds
+  // Switch on the LED if an 1 was received as first character
+  if (payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+PubSubClient client(server, 1883, callback, wifiClient);
+
+int publishInterval = 5000; // 5 seconds//Send adc every 5sc
 long lastPublishMillis;
-int lightSensorData = 0;
+
 void setup() {
-   Serial.begin(115200); Serial.println();
-    pinMode(A0, INPUT);
-   wifiConnect();
-   mqttConnect();
-   initManagedDevice();
+  Serial.begin(9600); Serial.println();
+  pinMode(LED_BUILTIN, OUTPUT);
+  wifiConnect();
+  mqttConnect();
 }
 
 void loop() {
-   if (millis() - lastPublishMillis > publishInterval) {
-      lightSensorData= analogRead(A0);
-      publishData(); 
-      lastPublishMillis = millis();
-   }
+  if (millis() - lastPublishMillis > publishInterval) {
+    publishData();
+    lastPublishMillis = millis();
+  }
 
-   if (!client.loop()) {
-      mqttConnect();
-      initManagedDevice();
-   }
+  if (!client.loop()) {
+    mqttConnect();
+  }
 }
 
 void wifiConnect() {
-   Serial.print("Connecting to "); Serial.print(ssid);
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-   } 
-   Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+
 }
 
 void mqttConnect() {
-   if (!!!client.connected()) {
-      Serial.print("Reconnecting MQTT client to "); Serial.println(server);
-      while (!!!client.connect(clientId, authMethod, token)) {
-         Serial.print(".");
-         delay(500);
-      }
-      Serial.println();
-   }
-}
-
-void initManagedDevice() {
-   if (client.subscribe(responseTopic)) {
+  if (!!!client.connected()) {
+    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+    while (!!!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    if (client.subscribe(cmdTopic)) {
       Serial.println("subscribe to responses OK");
-   } else {
+    } else {
       Serial.println("subscribe to responses FAILED");
-   }
-
-   if (client.subscribe(rebootTopic)) {
-      Serial.println("subscribe to reboot OK");
-   } else {
-      Serial.println("subscribe to reboot FAILED");
-   }
-
-   if (client.subscribe(updateTopic)) {
-      Serial.println("subscribe to update OK");
-   } else {
-      Serial.println("subscribe to update FAILED");
-   }
-
-   if (client.subscribe(fauCommandTopic)) {
-      Serial.println("subscribe to update OK");
-   } else {
-      Serial.println("subscribe to update FAILED");
-   }
-
-   StaticJsonBuffer<300> jsonBuffer;
-   JsonObject& root = jsonBuffer.createObject();
-   JsonObject& d = root.createNestedObject("d");
-   JsonObject& metadata = d.createNestedObject("metadata");
-   metadata["publishInterval"] = publishInterval;
-   JsonObject& supports = d.createNestedObject("supports");
-   supports["deviceActions"] = true;
-
-   char buff[300];
-   root.printTo(buff, sizeof(buff));
-   Serial.println("publishing device metadata:"); Serial.println(buff);
-   if (client.publish(manageTopic, buff)) {
-      Serial.println("device Publish ok");
-   } else {
-      Serial.print("device Publish failed:");
-   }
+    }
+    Serial.println();
+  }
 }
+
 
 void publishData() {
-   String payload = "{\"d\":{\"counter\":";
-   payload += millis() / 1000;
-   payload += ",\"light\":";
-   payload+= lightSensorData;
-   payload+="}}";
- 
-   Serial.print("Sending payload: "); Serial.println(payload);
- 
-   if (client.publish(publishTopic, (char*) payload.c_str())) {
-      Serial.println("Publish OK");
-   } else {
-      Serial.println("Publish FAILED");
-   }
-}
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
 
-void callback(char* topic, byte* payload, unsigned int payloadLength) {
-   Serial.print("callback invoked for topic: "); Serial.println(topic);
+  String payload = "{\"d\":{\"adc\":";
+  payload += String(sensorValue, DEC);
+  payload += "}}";
 
-   if (strcmp (responseTopic, topic) == 0) {
-      return; // just print of response for now 
-   }
+  Serial.print("Sending payload: "); Serial.println(payload);
 
-   if (strcmp (rebootTopic, topic) == 0) {
-      Serial.println("Rebooting...");
-      ESP.restart();
-   }
-
-   if (strcmp (updateTopic, topic) == 0) {
-      handleUpdate(payload); 
-   } 
-
-   if (strcmp (fauCommandTopic, topic) == 0) {
-      Serial.print("FAU command received: ");
-      Serial.println((char*)payload);
-   } 
-}
-
-void handleUpdate(byte* payload) {
-   StaticJsonBuffer<300> jsonBuffer;
-   JsonObject& root = jsonBuffer.parseObject((char*)payload);
-   if (!root.success()) {
-      Serial.println("handleUpdate: payload parse FAILED");
-      return;
-   }
-   Serial.println("handleUpdate payload:"); root.prettyPrintTo(Serial); Serial.println();
-
-   JsonObject& d = root["d"];
-   JsonArray& fields = d["fields"];
-   for(JsonArray::iterator it=fields.begin(); it!=fields.end(); ++it) {
-      JsonObject& field = *it;
-      const char* fieldName = field["field"];
-      if (strcmp (fieldName, "metadata") == 0) {
-         JsonObject& fieldValue = field["value"];
-         if (fieldValue.containsKey("publishInterval")) {
-            publishInterval = fieldValue["publishInterval"];
-            Serial.print("publishInterval:"); Serial.println(publishInterval);
-         }
-      }
-   }
+  if (client.publish(eventTopic, (char*) payload.c_str())) {
+    Serial.println("Publish OK");
+  } else {
+    Serial.println("Publish FAILED");
+  }
 }
 
 /*Servo servo;
